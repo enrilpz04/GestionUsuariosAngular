a7da882dee9566ea4e76828aa6ace52c0af0c5da
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-08-20 21:57:15 +0200

    Components and pages created
    Components and pages created

---
+++ src/app/components/usercard/usercard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usercard',
+  standalone: true,
+  imports: [],
+  templateUrl: './usercard.component.html',
+  styleUrl: './usercard.component.css'
+})
+export class UsercardComponent {
+
+}
