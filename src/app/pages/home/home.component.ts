508c1bff436811f35ac93c3fe3ee1ad37a4b30e5
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-08-25 10:04:47 +0200

    IStudent attr defined

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IStudent } from '../../interfaces/istudent.interface';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
 }
