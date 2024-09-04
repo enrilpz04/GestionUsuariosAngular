8c849305a56f6beaa15dc09ec7f3074f57830a98
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-09-04 16:23:58 +0200

    Header completed + navigation
    Header completed + navigation

--- src/app/components/usercard/usercard.component.ts
+++ src/app/components/usercard/usercard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IStudent } from '../../interfaces/istudent.interface';
 
 @Component({
   selector: 'app-usercard',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './usercard.component.css'
 })
 export class UsercardComponent {
-
+  @Input() student: any;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,91 @@
 import { Component } from '@angular/core';
 import { IStudent } from '../../interfaces/istudent.interface';
+import { UsercardComponent } from '../../components/usercard/usercard.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UsercardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   
+  arrStudents: IStudent[] = []
+
+  ngOnInit() {
+    this.arrStudents = [
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      },
+      {
+        _id: '1',
+        first_name: 'John',
+        last_name: 'Doe',
+        username: 'johndoe',
+        email: '',
+        image: 'https://i.pravatar.cc/500?u=clemente.alonzomayorga@peticiones.online',
+        password: '',
+      }
+  ]}
 }
