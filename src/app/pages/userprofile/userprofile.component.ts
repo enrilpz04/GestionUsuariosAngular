b0f56b42b2183e73877325b11b913a9a0c0ce08b
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-09-06 13:01:17 +0200

    User charged from API with the id in parameters
    User charged from API with the id in parameters

--- src/app/pages/userprofile/userprofile.component.ts
+++ src/app/pages/userprofile/userprofile.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IStudent } from '../../interfaces/istudent.interface';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { StudentsService } from '../../services/students.service';
 
 @Component({
   selector: 'app-userprofile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './userprofile.component.html',
   styleUrl: './userprofile.component.css'
 })
 export class UserprofileComponent {
+  student! : IStudent
+  activatedRoute = inject(ActivatedRoute)
+  studentsService = inject(StudentsService)
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(async (params: any) => {
+      let id = params.id
+      this.student = await this.studentsService.getById(id);
+    })
+    
+  }
 
 }
