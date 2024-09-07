d5974fd02554897f4844bd78a8ce651783f22dbc
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-09-07 17:10:59 +0200

    Data from the student show in the page and buttons with routes working
    Data from the student show in the page and buttons with routes working

--- src/app/pages/userprofile/userprofile.component.ts
+++ src/app/pages/userprofile/userprofile.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IStudent } from '../../interfaces/istudent.interface';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { StudentsService } from '../../services/students.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-userprofile',
@@ -20,7 +21,45 @@ export class UserprofileComponent {
       let id = params.id
       this.student = await this.studentsService.getById(id);
     })
-    
+  }
+
+  
+  deleteUser() {
+    Swal.fire({
+      title:
+        '¿Estás seguro de eliminar al usuario ' + this.student.first_name + '?',
+      text: 'No podrás revertir esto',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Aceptar',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.studentsService.delete(this.student._id).then((response) => {
+          if(response._id){
+            Swal.fire({
+              title: 'Usuario eliminado con éxito',
+              text: `El usuario ${this.student.first_name} ${this.student.last_name} ha sido eliminado.`,
+              icon: 'success',
+              confirmButtonColor: '#3085d6',
+              confirmButtonText: 'Volver',
+            });
+          } else {
+            throw new Error('Error al eliminar el usuario');
+          }
+        }).catch((error) => {
+          Swal.fire({
+            title: 'Error al eliminar el usuario',
+            text: 'No se pudo eliminar el usuario. Por favor, intenta nuevamente.',
+            icon: 'error',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Volver',
+          });
+        });
+      }
+    });
   }
 
 }
